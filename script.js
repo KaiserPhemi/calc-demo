3609b1c50e7c388744b5f095b91843ba4336568e
KaiserPhemi <kaiser.phemi@protonmail.ch>
2020-02-13 17:57:05 +0100

    ft(js)
    - add function to handle click events

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Calc loaded");
+let runningTotal = 0;
+let buffer = "0";
+let prevOperator;
+
+const screen = document.querySelector(".result");
+
+const buttonClick = value => {
+  if (value === "C") {
+    screen.innerText = buffer;
+  } else {
+    screen.innerText = value;
+  }
+};
+
+/**
+ * @desc gets the value from every item clicked
+ */
+const init = () => {
+  document.querySelector("#main-app").addEventListener("click", event => {
+    buttonClick(event.target.innerText);
+  });
+};
+
+init();
